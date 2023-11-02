f33f99f499df812a536745d78aef67d4975c0e72
1 <balabanov-03@list.ru>
2023-11-02 21:40:43 +0300

    Add includes and using in Tokenizer

--- Utilities/Tokenizer.m.cpp
+++ Utilities/Tokenizer.m.cpp
@@ -1,6 +1,25 @@
 module;
 
+#include <sstream>
+#include <string>
+#include <vector>
+#include <istream>
+#include <compare>
+#include <iterator>
+#include <ranges>
+#include <iostream>
+#include <algorithm>
+
 export module CalcUtilities:Tokenizer;
+
+using std::string;
+using std::istream_iterator;
+using std::back_inserter;
+using std::vector;
+using std::istream;
+
+namespace ranges = std::ranges;
+namespace views = std::views;
 
 export class Tokenizer
 {
