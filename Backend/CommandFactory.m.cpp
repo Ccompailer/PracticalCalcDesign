c9d8fe045947e1a24a85f27eb50226c8291072f5
1 <balabanov-03@list.ru>
2023-11-10 22:31:25 +0300

    End EnterNumber class impl in CoreCommands module

---
+++ Backend/CommandFactory.m.cpp
@@ -0,0 +1,13 @@
+module;
+
+#include <memory>
+#include <string>
+#include <set>
+#include <format>
+#include <unordered_map>
+#include <algorithm>
+#include <ranges>
+
+export module CalcBackend_CommandFactory;
+
+import CalcUtilities;
---
+++ Backend/CoreCommands.m.cpp
@@ -0,0 +1,79 @@
+module;
+
+#include <string>
+#include <stack>
+#include <cassert>
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+export module CalcBackend_CoreCommands;
+
+import CalcBackend_Stack;
+import CalcBackend_Command;
+import CalcUtilities;
+
+using std::vector;
+using std::string;
+
+#define CLONE(X) X* cloneImp() const override { return new X { *this }; }
+#define HELP(X) const char* helpMessageImp() const noexcept override { return X; }
+
+
+double eps = 1e-12;
+
+bool TopOfStackIsBetween(double lb, double ub) {
+    assert(lb <= ub);
+
+    auto v = Stack::Instance().GetElements(1);
+    auto d = v.back();
+
+    return d >= lb && d <= ub;
+}
+
+bool PassesPowerTest(double y, double x) {
+    if(y == 0 && x < 0)
+        return false;
+
+    if(double intPart; y < 0 && std::modf(x, &intPart) != 0.0)
+        return false;
+
+    return true;
+}
+
+export class EnterNumber : public Command
+{
+public:
+    explicit EnterNumber(double d)
+    : Command {}
+    , _number(d)
+    { }
+
+    explicit EnterNumber(const EnterNumber& rhs)
+    : Command(rhs)
+    , _number(rhs._number)
+    { }
+
+    ~EnterNumber() = default;
+
+private:
+    EnterNumber(EnterNumber&&) = delete;
+    EnterNumber& operator=(const EnterNumber&) = delete;
+    EnterNumber& operator=(EnterNumber&&) = delete;
+
+    void executeImp() noexcept override {
+        Stack::Instance().Push(_number);
+    }
+
+    void undoImp() noexcept override {
+        Stack::Instance().Pop();
+    }
+
+    CLONE(EnterNumber);
+    HELP("Adds a number to the stack");
+
+    double _number;
+};
+
+
+
