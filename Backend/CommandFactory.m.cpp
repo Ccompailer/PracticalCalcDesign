a1f03af725095b43c2650a24483e44e25b835130
1 <balabanov-03@list.ru>
2023-11-26 21:38:50 +0300

    Начал писать фабрику команд

--- Backend/CommandFactory.m.cpp
+++ Backend/CommandFactory.m.cpp
@@ -11,3 +11,25 @@ module;
 export module CalcBackend_CommandFactory;
 
 import CalcUtilities;
+import CalcBackend_Command;
+import CalcBackend_CoreCommands;
+
+using std::string;
+using std::unordered_map;
+using std::set;
+
+namespace CommandModule {
+
+    export class CommandFactory {
+    public:
+        static CommandFactory& Instance();
+        void RegisterCommand(const string& name, CommandPtr ptr);
+        CommandPtr DeregisterCommand(const string& name);
+        size_t GetNumberCommand() const { _factory.size(); }
+
+
+    private:
+        using Factory = unordered_map<string, CommandPtr>;
+        Factory _factory;
+    };
+}
