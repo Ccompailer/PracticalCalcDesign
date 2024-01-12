486da44911b25e24c9b56ff20a0650e4b394f3de
Ccompailer <balabanov-03@list.ru>
2024-01-12 23:22:05 +0300

    Finish implement WindowsDynamicLoader module

--- Backend/WindowsDynamicLoader.m.cpp
+++ Backend/WindowsDynamicLoader.m.cpp
@@ -1,6 +1,5 @@
 module;
 
-#include <cstdlib>
 #include <vector>
 #include <windows.h>
 #include <string>
@@ -15,6 +14,22 @@ using std::vector;
 
 namespace Calculator {
     export class WindowsDynamicLoader : public DynamicLoader {
+    public:
+        WindowsDynamicLoader();
+        ~WindowsDynamicLoader();
+
+    private:
+        HINSTANCE _handle;
 
     };
+
+    WindowsDynamicLoader::WindowsDynamicLoader()
+        : DynamicLoader {}, _handle {nullptr} {
+
+    }
+
+    WindowsDynamicLoader::~WindowsDynamicLoader() {
+        if (_handle)
+            FreeLibrary(_handle);
+    }
 }
