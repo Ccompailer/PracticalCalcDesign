1e125d5bb8b75fe5f35f138d397fb47c3628b932
Ccompailer <balabanov-03@list.ru>
2024-01-20 21:06:05 +0300

    A little bit refactoring code

--- Backend/WindowsFactory.cpp
+++ Backend/WindowsFactory.cpp
@@ -7,7 +7,6 @@ module WindowsFactory;
 import DynamicLoader;
 
 namespace Calculator {
-    class DynamicLoader;
 
     std::unique_ptr<DynamicLoader> WindowsFactory::CreatedDynamicLoader() {
         return nullptr; // Нужно будет сделать WindowsDynamicLoader который будет наследоваться от DynamicLoader
